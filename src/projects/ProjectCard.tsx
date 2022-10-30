3265fc1957a15e7d0ffcf628f5bedab9780e5c9c
dibabasu <dibabasu@gmail.com>
2022-10-31 01:11:58 +0530

    Till Lab 14

--- src/projects/ProjectCard.tsx
+++ src/projects/ProjectCard.tsx
@@ -7,10 +7,14 @@ function formatDescription(description: string): string {
 
 interface ProjectCardProps {
   project: Project;
+  onEdit: (project: Project) => void;
 }
 
 function ProjectCard(props: ProjectCardProps) {
-  const { project } = props;
+  const { project, onEdit } = props;
+  const handleEditClick = (projectBeingEdited: Project) => {
+      onEdit(projectBeingEdited);
+      };
   return (
     <div className="card">
       <img src={project.imageUrl} alt={project.name} />
@@ -20,6 +24,14 @@ function ProjectCard(props: ProjectCardProps) {
         </h5>
         <p>{formatDescription(project.description)}</p>
         <p>Budget : {project.budget.toLocaleString()}</p>
+        <button   className=" bordered"
+          onClick={() => {
+            handleEditClick(project);
+          }}
+        >
+        <span className="icon-edit "></span>
+        Edit
+        </button>
       </section>
     </div>
   );
--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,12 +1,18 @@
+import React, { Fragment } from 'react';
 import { MOCK_PROJECTS } from './MockProjects';
 import ProjectList from './ProjectList';
+import { Project } from './Project';
 function ProjectsPage() {
-  return (
-   <>
-     <h1>Projects</h1>
-     <ProjectList projects={MOCK_PROJECTS} />
-   </>
-  );
-}
-
+    const saveProject = (project: Project) => {
+      console.log('Saving project: ', project);
+   };
+   return (
+    <Fragment>
+       <h1>Projects</h1>
+       <ProjectList
+       onSave={saveProject}
+         projects={MOCK_PROJECTS} />
+    </Fragment>
+ );
+   }
 export default ProjectsPage;
