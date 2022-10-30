6d8867b6734fecb20944c7f34110d92dd22b2482
Dibabasu Bhattacharya <dibabasu@gmail.com>
2022-10-30 23:51:01 +0530

    initial commit

---
+++ src/projects/ProjectList.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Project } from './Project'; 
+import ProjectCard from './ProjectCard';
+
+function ProjectList ({ projects }) {
+    const items = projects.map(project => (
+      <div key={project.id} className="cols-sm">
+
+      <ProjectCard project={project}></ProjectCard>
+      </div>
+    ));
+    return <div className="row">{items}</div>;
+
+}
+
+export default ProjectList;
