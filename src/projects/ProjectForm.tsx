67cfd413ea16ffad1e03e75189ac6ff62ed5ec33
Dibabasu Bhattacharya <dibabasu@gmail.com>
2022-11-06 00:58:42 +0530

    Lab 26

--- src/projects/ProjectForm.tsx
+++ src/projects/ProjectForm.tsx
@@ -1,14 +1,19 @@
 import React, { SyntheticEvent, useState } from 'react';
 import { Project } from './Project';
+import { useDispatch } from 'react-redux';
+import { saveProject } from './state/projectActions';
+import { ThunkDispatch } from 'redux-thunk';
+import { ProjectState } from './state/projectTypes';
+import { AnyAction } from 'redux';
+
+
  interface ProjectFormProps {
    project: Project;
-   onSave: (project: Project) => void;
    onCancel: () => void;
  }
 
   function ProjectForm({ 
     project: initialProject,
-    onSave, 
     onCancel }: ProjectFormProps) {
       const [project, setProject] = useState(initialProject);
        const [errors, setErrors] = useState({
@@ -16,10 +21,11 @@ import { Project } from './Project';
            description: '',
            budget: '',
          });
+      const dispatch = useDispatch<ThunkDispatch<ProjectState, any, AnyAction>>();
       const handleSubmit = (event: SyntheticEvent) => {
         if (!isValid()) return;
         event.preventDefault();
-        onSave(project);
+        dispatch(saveProject(project));
       }
         const handleChange = (event: any) => {
             const { type, name, value, checked } = event.target;
--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,56 +1,34 @@
-import React, { 
-   Fragment,
-   useState,
-   useEffect } from 'react'; 
+import React, { Fragment, useEffect } from 'react';
 import ProjectList from './ProjectList';
 import { Project } from './Project';
 import { projectAPI } from './projectAPI';
+import { useSelector, useDispatch } from 'react-redux';
+import { AppState } from '../state';
+import { loadProjects } from './state/projectActions';
+import { AnyAction } from 'redux';
+import { ThunkDispatch } from 'redux-thunk';
+import { ProjectState } from './state/projectTypes';
 
 function ProjectsPage() {
-   const [projects, setProjects] = useState<Project[]>([]);
-   const [loading, setLoading] = useState(false);
-   const [error, setError] = useState<string | undefined>(undefined);
-   const [currentPage, setCurrentPage] = useState(1);
-   const handleMoreClick = () => {
-          setCurrentPage((currentPage) => currentPage + 1);
-        };
-     useEffect(() => {
-          async function loadProjects() {
-            setLoading(true);
-            try {
-              const data = await projectAPI.get(currentPage);
-              setError('');
-              if (currentPage === 1) {
-                         setProjects(data);
-                       } else {
-                         setProjects((projects) => [...projects, ...data]);
-                       }
-            }
-             catch (e) {
-              if (e instanceof Error) {
-                setError(e.message);
-              }
-              } finally {
-              setLoading(false);
-            }
-          }
-          loadProjects();
-         }, [currentPage]);
-    const saveProject = (project: Project) => {
-      projectAPI
-     .put(project)
-     .then((updatedProject) => {
-       let updatedProjects = projects.map((p: Project) => {
-         return p.id === project.id ? new Project(updatedProject) : p;
-       });
-       setProjects(updatedProjects);
-     })
-     .catch((e) => {
-        if (e instanceof Error) {
-         setError(e.message);
-        }
-     });
-   };
+  const loading = useSelector(
+    (appState: AppState) => appState.projectState.loading
+  );
+  const projects = useSelector(
+    (appState: AppState) => appState.projectState.projects
+  );
+  const error = useSelector(
+    (appState: AppState) => appState.projectState.error
+  );
+  const currentPage = useSelector(
+    (appState: AppState) => appState.projectState.page
+  );
+  const dispatch = useDispatch<ThunkDispatch<ProjectState, any, AnyAction>>();
+  useEffect(() => {
+    dispatch<any>(loadProjects(1));
+  }, [dispatch]);
+  const handleMoreClick = () => {
+    dispatch(loadProjects(currentPage + 1));
+  };
    return (
     <Fragment>
        <h1>Projects</h1>
@@ -66,9 +44,7 @@ function ProjectsPage() {
           </div>
         </div>
       )}
-       <ProjectList 
-       onSave={saveProject} 
-       projects={projects} />
+       <ProjectList projects={projects} />
               {!loading && !error && (
         <div className="row">
           <div className="col-sm-12">
